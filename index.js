f8e69566a0e5944b1235ff1214da8c155e59cc94
venom <abdo30201080101694@gmail.com>
2024-01-18 14:22:48 +0200

    Calculate years, months, and days

--- index.js
+++ index.js
@@ -17,4 +17,13 @@ button.addEventListener("click", () => {
   let today = Date.now();
   //calc the full age
   let ageInMilliseconds = today - birthdate;
+  // Calculate years, months, and days
+  const years = Math.floor(ageInMilliseconds / (365.25 * 24 * 60 * 60 * 1000));
+  const months = Math.floor(
+    (ageInMilliseconds % (365.25 * 24 * 60 * 60 * 1000)) /
+      (30.44 * 24 * 60 * 60 * 1000)
+  );
+  const days = Math.floor(
+    (ageInMilliseconds % (30.44 * 24 * 60 * 60 * 1000)) / (24 * 60 * 60 * 1000)
+  );
 });
