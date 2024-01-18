46bd21a46832c193755be5a1b91138232435f22f
venom <abdo30201080101694@gmail.com>
2024-01-18 14:09:39 +0200

    get the 3 inputs and add event to the button

---
+++ index.js
@@ -0,0 +1,6 @@
+let button = document.querySelector(".calc-button");
+button.addEventListener("click", () => {
+  let daysInput = document.getElementById("day").value;
+  let monthsInput = document.getElementById("month").value;
+  let yearsInput = document.getElementById("year").value;
+});
