1d996f380dbaa9405f9714f37217f64afbc49a5a
venom <abdo30201080101694@gmail.com>
2024-01-18 14:10:11 +0200

    create new date from the 3 inputs

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ button.addEventListener("click", () => {
   let daysInput = document.getElementById("day").value;
   let monthsInput = document.getElementById("month").value;
   let yearsInput = document.getElementById("year").value;
+  let fullDate = `${monthsInput}-${daysInput}-${yearsInput}`;
+  console.log(new Date(fullDate));
 });
