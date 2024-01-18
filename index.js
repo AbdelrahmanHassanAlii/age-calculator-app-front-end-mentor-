c515b9b174ae90d1a3ea225bbb73c3803fe00e48
venom <abdo30201080101694@gmail.com>
2024-01-18 14:18:07 +0200

    Check if a valid fullDate is selected

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 let button = document.querySelector(".calc-button");
 button.addEventListener("click", () => {
+  // get the 3 inputs
   let daysInput = document.getElementById("day").value;
   let monthsInput = document.getElementById("month").value;
   let yearsInput = document.getElementById("year").value;
-  let fullDate = `${monthsInput}-${daysInput}-${yearsInput}`;
-  console.log(new Date(fullDate));
+  //create new date
+  let fullDate = new Date(`${monthsInput}-${daysInput}-${yearsInput}`);
+  // Check if a valid fullDate is selected
+  if (isNaN(fullDate.getTime())) {
+    alert("Please enter a valid birthdate.");
+    return;
+  }
+  console.log(fullDate.getTime());
+  //get date now
+  let currentDate = Date.now();
+  console.log(currentDate);
 });
