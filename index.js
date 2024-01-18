bc4d9afc4b38c9582c3a2ba2076420a75739539d
venom <abdo30201080101694@gmail.com>
2024-01-18 18:02:01 +0200

    add handle for not NAN inputs

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let button = document.querySelector(".calc-button button");
 button.addEventListener("click", () => {
+  let trueDay = 1;
+  let trueMonth = 1;
+  let trueYear = 1;
   // get the 3 inputs
   let daysInput = document.getElementById("day").value;
   let monthsInput = document.getElementById("month").value;
@@ -25,15 +28,41 @@ button.addEventListener("click", () => {
     document.querySelector(
       'label[for="day"]'
     ).style.color = `hsl(0, 100%, 67%)`;
+    trueDay = 0;
   }
-  //cheek if month input is empty
+  //cheek if day input isnot number
+  if (isNaN(daysInput)) {
+    document.querySelector(
+      `.for-day`
+    ).innerHTML = `This field is should be a number`;
+    document.querySelector(`.for-day`).style.color = `hsl(0, 100%, 67%)`;
+    document.querySelector("#day").style.borderColor = "hsl(0, 100%, 67%)";
+    document.querySelector(
+      'label[for="day"]'
+    ).style.color = `hsl(0, 100%, 67%)`;
+    trueDay = 0;
+  }
   if (monthsInput === ``) {
+    //cheek if month input is empty
     document.querySelector(`.for-month`).innerHTML = `This field is required`;
     document.querySelector(`.for-month`).style.color = `hsl(0, 100%, 67%)`;
     document.querySelector("#month").style.borderColor = "hsl(0, 100%, 67%)";
     document.querySelector(
       'label[for="month"]'
     ).style.color = `hsl(0, 100%, 67%)`;
+    trueMonth = 0;
+  }
+  //cheek if month input isnot number
+  if (isNaN(monthsInput)) {
+    document.querySelector(
+      `.for-month`
+    ).innerHTML = `This field is should be a number`;
+    document.querySelector(`.for-month`).style.color = `hsl(0, 100%, 67%)`;
+    document.querySelector("#month").style.borderColor = "hsl(0, 100%, 67%)";
+    document.querySelector(
+      'label[for="month"]'
+    ).style.color = `hsl(0, 100%, 67%)`;
+    trueMonth = 0;
   }
   //cheek if year input is empty
   if (yearsInput === ``) {
@@ -43,32 +72,46 @@ button.addEventListener("click", () => {
     document.querySelector(
       'label[for="year"]'
     ).style.color = `hsl(0, 100%, 67%)`;
+    trueYear = 0;
   }
-  //create new date
-  let birthdate = new Date(
-    `${monthsInput}-${daysInput}-${yearsInput}`
-  ).getTime();
-  // Check if a valid fullDate is selected
-  if (isNaN(birthdate)) {
-    //   alert("Please enter a valid birthdate.");
-    return;
+  //cheek if years input isnot number
+  if (isNaN(yearsInput)) {
+    document.querySelector(
+      `.for-year`
+    ).innerHTML = `This field is should be a number`;
+    document.querySelector(`.for-year`).style.color = `hsl(0, 100%, 67%)`;
+    document.querySelector("#year").style.borderColor = "hsl(0, 100%, 67%)";
+    document.querySelector(
+      'label[for="year"]'
+    ).style.color = `hsl(0, 100%, 67%)`;
+    trueYear = 0;
   }
-  //get date now
-  let today = Date.now();
-  //calc the full age
-  let ageInMilliseconds = today - birthdate;
-  // Calculate years, months, and days
-  const years = Math.floor(ageInMilliseconds / (365.25 * 24 * 60 * 60 * 1000));
-  const months = Math.floor(
-    (ageInMilliseconds % (365.25 * 24 * 60 * 60 * 1000)) /
-      (30.44 * 24 * 60 * 60 * 1000)
-  );
-  const days = Math.floor(
-    (ageInMilliseconds % (30.44 * 24 * 60 * 60 * 1000)) / (24 * 60 * 60 * 1000)
-  );
 
-  //set new values of years, months and days
-  document.querySelector(`.years-output-num`).innerHTML = years;
-  document.querySelector(`.months-output-num`).innerHTML = months;
-  document.querySelector(`.days-output-num`).innerHTML = days;
+  if (trueDay && trueMonth && trueYear) {
+    //create new date
+    let birthdate = new Date(
+      `${monthsInput}-${daysInput}-${yearsInput}`
+    ).getTime();
+    //get date now
+    let today = Date.now();
+    //calc the full age
+    let ageInMilliseconds = today - birthdate;
+    // Calculate years, months, and days
+    const years = Math.floor(
+      ageInMilliseconds / (365.25 * 24 * 60 * 60 * 1000)
+    );
+    const months = Math.floor(
+      (ageInMilliseconds % (365.25 * 24 * 60 * 60 * 1000)) /
+        (30.44 * 24 * 60 * 60 * 1000)
+    );
+    const days = Math.floor(
+      (ageInMilliseconds % (30.44 * 24 * 60 * 60 * 1000)) /
+        (24 * 60 * 60 * 1000)
+    );
+
+    //set new values of years, months and days
+    document.querySelector(`.years-output-num`).innerHTML = years;
+    document.querySelector(`.months-output-num`).innerHTML = months;
+    document.querySelector(`.days-output-num`).innerHTML = days;
+  }
 });
